9b95ae275ed9ea3007c45dc636f4ebeaadbd4c70
ranjanirbu <ranjani.r.bu@gmail.com>
2025-11-08 21:22:38 +0530

    Add files via upload
    DataType HomeAssignment Day1 Commited

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Chrome {
+	
+	public static void main(String args[])
+	{
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("Chrome Version : " +version);
+		System.out.println("Chrome Developer : " +developer);
+		System.out.println("Chrome ReleaseYear : " +releaseYear);
+		System.out.println("Chrome ShortcutKey : " +shortcutKey);
+	}
+
+}
